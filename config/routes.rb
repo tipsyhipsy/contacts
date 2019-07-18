1fc841ff25bf02c6eb23ce5e1320917c49a1f3fe
setsuto horikawa <ralralph@gmail.com>
2019-07-19 00:37:14 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/contacts', to: 'contacts#new'
+  get '/new', to: 'contacts#new'
+  get '/create', to: 'contacts#create'
 end
