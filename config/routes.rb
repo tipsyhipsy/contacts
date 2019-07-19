66e47a2e8b475541a3b07dedd29cd1f692cd26f3
setsuto horikawa <ralralph@gmail.com>
2019-07-19 16:46:02 +0900

    root add

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # get '/contacts', to: 'contacts#new'
   # get '/new', to: 'contacts#new'
   # get '/create', to: 'contacts#create'
+  root to: 'contacts#index'
   resources :contacts
 end
